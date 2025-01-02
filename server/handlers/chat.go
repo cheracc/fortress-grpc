df7090d729296b31cbd087fc1d195d099a85d7e4
cheracc <cheracc.me@gmail.com>
2025-01-02 08:18:34 -0500

    added thread-safety to chat.go

--- server/handlers/chat.go
+++ server/handlers/chat.go
@@ -3,30 +3,57 @@ package handlers
 import (
 	"context"
 	"fmt"
+	"slices"
+	"sync"
 	"time"
 
 	"github.com/cheracc/fortress-grpc"
 	fgrpc "github.com/cheracc/fortress-grpc/grpc"
 )
 
+const (
+	purgeInterval = 1 * time.Minute
+)
+
 type ChatHandler struct {
 	fgrpc.UnimplementedChatServer
 	*AuthHandler
 	*fortress.Logger
-	ChatChannel
+	*ChatChannel
 }
 
+// ChatChannel represents a chat channel. Its methods are thread-safe
 type ChatChannel struct {
-	members []ChannelMember
+	*sync.RWMutex
+	members []*ChannelMember
 }
 
-func (c *ChatChannel) addMember(m ChannelMember, playerName string) {
-	c.sendMessage(fmt.Sprintf("%s has joined chat", playerName), "SERVER")
+func (c *ChatChannel) addMember(m *ChannelMember, playerName string) {
+	c.sendMessage(fmt.Sprintf("%s has joined chat", playerName), "SERVER") // send this before adding the player so the new player doesn't get it (they already get their own msg)
+	c.Lock()
 	c.members = append(c.members, m)
+	c.Unlock()
 }
 func (c *ChatChannel) sendMessage(message string, playerName string) {
+	c.RLock()
 	for _, m := range c.members {
 		m.stream.Send(&fgrpc.ChatMessage{Message: message, SendingPlayerName: playerName})
+	}
+	c.RUnlock()
+}
+func (c *ChatChannel) removeMember(playerId string) {
+	toRemove := -1
+	c.RLock()
+	for i, m := range c.members {
+		if m.playerId == playerId {
+			toRemove = i
+			break
+		}
+	}
+	if toRemove >= 0 {
+		c.Lock()
+		c.members = slices.Delete(c.members, toRemove, toRemove+1)
+		c.Unlock()
 	}
 }
 
@@ -37,7 +64,18 @@ type ChannelMember struct {
 }
 
 func NewChatHandler(logger *fortress.Logger, auth *AuthHandler) *ChatHandler {
-	h := &ChatHandler{fgrpc.UnimplementedChatServer{}, auth, logger, ChatChannel{[]ChannelMember{}}}
+	h := &ChatHandler{fgrpc.UnimplementedChatServer{},
+		auth,
+		logger,
+		&ChatChannel{&sync.RWMutex{}, make([]*ChannelMember, 0)}}
+
+	go func() {
+		time.Sleep(20 * time.Second)
+		for {
+			h.PurgeInactives()
+			time.Sleep(purgeInterval)
+		}
+	}()
 
 	return h
 }
@@ -49,7 +87,7 @@ func (h *ChatHandler) JoinChannel(req *fgrpc.ChatRequest, stream fgrpc.Chat_Join
 		return h.Error("invalid or expired session token")
 	}
 
-	member := ChannelMember{playerId, stream, false}
+	member := &ChannelMember{playerId, stream, false}
 	h.addMember(member, h.GetPlayerNameFromId(playerId, false))
 	for {
 		if member.closed {
@@ -70,4 +108,19 @@ func (h *ChatHandler) SendMessage(ctx context.Context, msg *fgrpc.ChatMessage) (
 
 	h.ChatChannel.sendMessage(msg.GetMessage(), h.GetPlayerNameFromId(playerId, false))
 	return nil, nil
+}
+
+// PurgeInactives will check each channel member to see if they are still online, then remove those who are not
+func (h *ChatHandler) PurgeInactives() {
+	inactives := []string{}
+	h.RLock()
+	for _, m := range h.members {
+		if !h.IsOnline(PlayerFilter{playerId: m.playerId}) {
+			inactives = append(inactives, m.playerId)
+		}
+	}
+	h.RUnlock()
+	for _, id := range inactives {
+		h.removeMember(id)
+	}
 }
